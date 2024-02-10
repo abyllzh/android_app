9159f74be48f643d038ff2cece9ea4c74d4d3368
Abylay Zhangabay <abylayzhangabay@MacBook-Air-Abylay-2.local>
2024-02-10 21:40:16 +0600

    Initial commit

---
+++ app/src/main/java/com/example/abylay_lab_3_intents/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.abylay_lab_3_intents
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnBio= findViewById<Button>(R.id.button2)
+        btnBio.setOnClickListener{
+            intent= Intent(applicationContext, Second_Screen::class.java)
+            startActivity(intent)
+        }
+        val btnprofile= findViewById<Button>(R.id.button3)
+        btnprofile.setOnClickListener {
+            val newlink= Intent(Intent.ACTION_VIEW)
+            newlink.data= Uri.parse("https://harimohanrai.wordpress.com/")
+            startActivity(newlink)
+        }
+        val btncamera= findViewById<Button>(R.id.button4)
+        btncamera.setOnClickListener {
+            val opencamera= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(opencamera)
+        }
+
+
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Abylay_Lab_3_intents"
+include(":app")
